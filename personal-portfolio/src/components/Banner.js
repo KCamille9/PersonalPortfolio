94584ee9bb684953df245cc8c9bb5d93c78e0606
KCamille9 <bgi_camille@hotmail.com>
2022-07-17 17:11:37 -0600

    starting conditions for banner animation

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/headerImg.svg";
 
 
 export const Banner = () => {
+
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer", "Wed Designer", "UI/UX Designer"]; 
+    const [text, setText] = useState(''); // this is the starting letter
+    const period = 2000; //time
+
     return (
         <section className="banner" id="home">
             <Container>
@@ -19,7 +27,7 @@ export const Banner = () => {
                     </Col>
 
                     <Col xs={12} md={6} xl={5}>
-                        <img src={''} alt="Header img"/>
+                        <img src={headerImg} alt="Header img"/>
                     </Col>
                 </Row>
 
